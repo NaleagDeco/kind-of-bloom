c2c2b6d7e34f980d3420e43d781d6287839535f8
Gaelan D'costa <gdcosta@gmail.com>
2014-11-30 23:06:51 -0500

    Adding calculation for bloom filter's false positive rate.

--- src/kind_of_bloom/core.clj
+++ src/kind_of_bloom/core.clj
@@ -8,3 +8,6 @@
 
 (defn hash-functions [bf]
   (bf :hashes))
+
+(defn false-positive-rate [num-hashes filter-length expected-amount]
+  (- 1 (exp (/ (* -1 num-hashes expected-amount) filter-length))))
